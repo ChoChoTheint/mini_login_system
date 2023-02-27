e8ad4dfd4599e80854f597e5247b59484b2298f7
Cho Cho Theint <chochotheint19@gmail.com>
2023-02-27 20:43:28 +0630

    27FecNight

--- validation.php
+++ validation.php
@@ -8,23 +8,78 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <div class="card">
-            <div class="card-body">
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+    <?php
+    $errorName = $errorEmail = $errorPhone = $errorAddress = "";
+    $name = $email = $phone = $address = "";
+    if(isset($_POST['savebtn'])){
+        if($_POST['name'] ==null || $_POST['name'] == "" || empty($_POST['name']) ){
+            $errorName = "Please fill name field!";
+        }else{
+            $name = $_POST['name'];  
+        }
+
+        if($_POST['email'] ==null || $_POST['email'] == "" || empty($_POST['email']) ){
+            $errorEmail = "Please fill email field!";
+        }else{
+            $email = $_POST['email'];  
+        }
+
+        if($_POST['address'] ==null || $_POST['address'] == "" || empty($_POST['address']) ){
+            $errorAddress = "Please fill address field!";
+        }else{
+            $address = $_POST['address'];  
+        }
+
+        if($_POST['phone'] ==null || $_POST['phone'] == "" || empty($_POST['phone']) ){
+            $errorPhone = "Please fill phone field!";
+        }else{
+            $phone = $_POST['phone'];  
+        }
+
+        if($name != "" && $email != "" && $address != "" && $phone != "") {
+            echo $name . "<br/>";
+            echo $email . "<br/>";
+            echo $address . "<br/>";
+            echo $phone . "<br/>";
+        }
+    }
+
+    ?>
+
+
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-6 offset-3 shadow">
+                <form action="" method="POST">
+                    <div class="my-3 px-5">
+                        <label for="">Name</label>
+                        <input type="text" name="name" class="form-control" placeholder="Enter Name">
+                        <span class="text-danger"><?php echo $errorName; ?></span>
+                    </div>
+                    <div class="my-3 px-5">
+                        <label for="">Email</label>
+                        <input type="email" name="email" class="form-control" placeholder="Eg: example@gmail.com">
+                        <span class="text-danger"><?php echo $errorEmail; ?></span>
+                    </div>
+
+                    <div class="my-3 px-5">
+                        <label for="">Phone</label>
+                        <input type="number" name="phone" class="form-control" placeholder="Eg: 09.........">
+                        <span class="text-danger"><?php echo $errorPhone; ?></span>
+                    </div>
+                    <div class="my-3 px-5">
+                        <label for="">Address</label>
+                        <textarea name="address" class="form-control" id="" cols="30" rows="10" placeholder="Enter address"> </textarea>
+                        <span class="text-danger"><?php echo $errorAddress; ?></span>
+                    </div>
+                    <div class="my-3 px-5 float-end">
+                        <button name="savebtn" type="submit" class="btn text-white bg-dark">Save</button>
+                    </div>
+                </form>
             </div>
         </div>
+
     </div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
